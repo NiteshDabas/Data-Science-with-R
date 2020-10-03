df3b82430764579060ebbbb413b6f8725d80e38f
Nitesh Dabas <dabas.nitesh@gmail.com>
2020-10-03 12:22:55 +0200

    Reading data in R - advanced

---
+++ DataCamp_ReadDataInR2.R
@@ -0,0 +1,169 @@
+#====================#====================##====================#====================#
+#Database connection using R
+#====================#====================##====================#====================#
+# # Load the DBI package
+library(DBI)
+## Connect to the MySQL database: con
+con <- dbConnect(RMySQL::MySQL(), 
+                  dbname = "tweater", 
+                  host = "courses.csrrinzqubik.us-east-1.rds.amazonaws.com", 
+                  port = 3306,
+                  user = "student",
+                  password = "datacamp")
+# # Get table names
+table_names <- dbListTables(con)
+# # Import all tables 
+tables <- lapply(table_names, dbReadTable, conn = con)
+# # Print out tables
+tables
+
+#====================#====================##====================#====================#
+#Selective Importing from databases using R
+#====================#====================##====================#====================#
+library(DBI)
+con <- dbConnect(RMySQL::MySQL(),
+                  dbname = "tweater",
+                  host = "courses.csrrinzqubik.us-east-1.rds.amazonaws.com",
+                  port = 3306,
+                  user = "student",
+                  password = "datacamp")
+# Create data frame specific
+specific<-dbGetQuery(con, "select message from comments where tweat_id = 77")
+# Print specific
+specific
+
+# # Connect to the database
+library(DBI)
+con <- dbConnect(RMySQL::MySQL(),
+                  dbname = "tweater",
+                  host = "courses.csrrinzqubik.us-east-1.rds.amazonaws.com",
+                  port = 3306,
+                  user = "student",
+                  password = "datacamp")
+# # Create the data frame  long_tweats
+long_tweats <- dbGetQuery(con,"select post, date from tweats where CHAR_LENGTH(post)>40")
+# # Print long_tweats
+print(long_tweats)
+# # Disconnect from the database
+dbDisconnect(con)
+
+#====================#====================##====================#====================#
+# Importing data from Web using R
+#====================#====================##====================#====================#
+# # Load the readr package
+library(readr)
+# # Import the csv file: pools
+url_csv <- "http://s3.amazonaws.com/assets.datacamp.com/production/course_1478/datasets/swimming_pools.csv"
+pools<-read_csv(url_csv)
+# # Import the txt file: potatoes
+url_delim <- "http://s3.amazonaws.com/assets.datacamp.com/production/course_1478/datasets/potatoes.txt"
+potatoes<-read_tsv(url_delim)
+# # Print pools and potatoes
+pools
+potatoes
+
+# # https URL to the swimming_pools csv file.
+url_csv <- "https://s3.amazonaws.com/assets.datacamp.com/production/course_1478/datasets/swimming_pools.csv"
+# # Import the file using read.csv(): pools1
+pools1<-read.csv(url_csv)
+# # Load the readr package
+library(readr)
+# # Import the file using read_csv(): pools2
+pools2<-read_csv(url_csv)
+# # Print the structure of pools1 and pools2
+str(pools1)
+str(pools2)
+
+#gdata can handle .xls files that are on the internet. readxl can't. So download the file first and then call read_excel(readxl package) on them
+# # Load the readxl and gdata package
+library(readxl)
+library(gdata)
+# # Specification of url: url_xls
+url_xls <- "http://s3.amazonaws.com/assets.datacamp.com/production/course_1478/datasets/latitude.xls"
+# # Import the .xls file with gdata: excel_gdata
+excel_gdata <- read.xls(url_xls)
+# # Download file behind URL, name it local_latitude.xls
+download.file(url_xls, "local_latitude.xls")
+# # Import the local .xls file with readxl: excel_readxl
+excel_readxl <- read_excel("local_latitude.xls")
+
+# # https URL to the wine RData file.
+url_rdata <- "https://s3.amazonaws.com/assets.datacamp.com/production/course_1478/datasets/wine.RData"
+# # Download the wine file to your working directory
+download.file(url_rdata, destfile = "wine_local.RData")
+# # Load the wine data into your workspace using load()
+load("wine_local.RData")
+# # Print out the summary of the wine data
+summary(wine)
+
+##httr package
+# httr is already loaded
+library(httr)
+# # Get the url
+url <- "http://www.omdbapi.com/?apikey=72bc447a&t=Annie+Hall&y=&plot=short&r=json"
+resp<-GET(url)
+# # Print resp
+resp
+# # Print content of resp as text
+content(resp, as = "text")
+content(resp)
+# # Print content of resp
+resp
+
+#====================#====================##====================#====================#
+# Importing data from JASON/APIs using R
+#====================#====================##====================#====================#
+
+#Jsonlite package => JSONLite fromJSON() function converts a JSON data into nicely structured R table data. It takes url/or data as input parameter.
+# # Load the jsonlite package
+library(jsonlite)
+# # wine_json is a JSON
+wine_json <- '{"name":"Chateau Migraine", "year":1997, "alcohol_pct":12.4, "color":"red", "awarded":false}'
+# # Convert wine_json into a list: wine
+wine <- fromJSON(wine_json)
+# # Print structure of wine
+str(wine)
+
+#toJSON() function converts a nicely structured R table data into JSON data 
+# # jsonlite is already loaded
+# # URL pointing to the .csv file
+url_csv <- "http://s3.amazonaws.com/assets.datacamp.com/production/course_1478/datasets/water.csv"
+# # Import the .csv file located at url_csv
+water <- read.csv(url_csv, stringsAsFactors = FALSE)
+# # Convert the data file according to the requirements
+water_json <- toJSON(water)
+# # Print out water_json
+water_json
+
+# #Minify : a minified format of data
+# #Prettify : a pretty format with indentation, whitespace and new lines
+# # jsonlite is already loaded
+# 
+# # Convert mtcars to a pretty JSON: pretty_json
+pretty_json <- toJSON(mtcars, pretty = TRUE)
+# # Print pretty_json
+pretty_json
+# 
+# # Minify pretty_json: mini_json
+mini_json <- toJSON(mtcars)
+# # Print mini_json
+mini_json
+
+#====================#====================##====================#====================#====================#
+# haven/foreign packages in R => importing data from other statistical packages SAS, STATA, SPSS
+#====================#====================##====================#====================#====================#
+# Load the haven package
+library(haven)
+# Import sales.sas7bdat: sales
+# Display the structure of sales
+sales <- read_sas("sales.sas7bdat")
+str(sales)
+# haven is already loaded
+# Import the data from the URL: sugar
+# Structure of sugar
+sugar <- read_dta("http://assets.datacamp.com/production/course_1478/datasets/trade.dta")
+# Convert values in Date column to dates
+str(sugar)
+sugar$Date <- as.Date(as_factor(sugar$Date))
+# Structure of sugar again
+str(sugar)
